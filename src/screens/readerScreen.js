5da0e0f21bf31f0a3484201ecb508db71ce34f3c
roycechua23 <royce236@gmail.com>
2020-07-23 15:59:30 +0800

    Add initial code for readerScreen

---
+++ src/screens/readerScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const readerScreen = () => {
+    return (
+        <View>
+            <Text>Reader Screen</Text>
+            {/* TODO#1: Render the text/book content here. */}
+            <Text>Display books here!</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default readerScreen;
