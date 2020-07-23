cbb60162e5c5ff4384779bdf3e6ed57f52e3a1ca
roycechua23 <royce236@gmail.com>
2020-07-23 22:59:24 +0800

    add margin to homescreen container

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     Container: {
-        margin: 15,
+        margin: 100,
         justifyContent:'flex-start',
         alignItems: 'center'
     },
