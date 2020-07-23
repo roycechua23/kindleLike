dfff080522e5acf979f7406d69e6b9cdf70abbf9
roycechua23 <royce236@gmail.com>
2020-07-23 15:59:42 +0800

    Add initial code for homeScreen

---
+++ src/screens/homeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const homeScreen = () => {
+    return (
+        <View>
+            <Text>Home Screen</Text>
+            {/* TODO#2: Render the text/book content here. */}
+            <Text>Render Book Files here</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default readerScreen;
