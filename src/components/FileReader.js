7eadadd6f34c4e87064ce31b19c3c307e4a926b5
roycechua23 <royce236@gmail.com>
2020-07-23 22:38:40 +0800

    Add FileReader Component

---
+++ src/components/FileReader.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Text, StyleSheet, TouchableOpacity } from 'react-native';
+import * as DocumentPicker from 'expo-document-picker';
+import * as FileSystem from 'expo-file-system';
+
+const FileReader = ({title, onFileSubmit}) => {
+    const readBookFile = () => {
+        DocumentPicker.getDocumentAsync().then(
+            (file) => {
+                FileSystem.readAsStringAsync(file.uri).then(
+                    (strVal) => onFileSubmit(strVal)
+                );
+            }
+        );
+    }
+
+    return (
+        <TouchableOpacity onPress={readBookFile}>
+                <Text style={styles.ButtonTextStyle}>{title}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    ButtonTextStyle: {
+        fontSize: 30,
+        color: '#ffff',
+        backgroundColor: '#f5a142',
+        borderRadius: 15,
+        padding: 15,
+    },
+});
+
+export default FileReader;
