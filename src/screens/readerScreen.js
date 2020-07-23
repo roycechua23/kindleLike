7364a82e6e0ea1eea3f91c214b79e912f5a99182
roycechua23 <royce236@gmail.com>
2020-07-23 21:37:52 +0800

    put book generation into a separate component

--- src/screens/readerScreen.js
+++ src/screens/readerScreen.js
@@ -2,67 +2,16 @@ import React, { useCallback, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ViewPager from '@react-native-community/viewpager';
 // import { story } from '../constants/constants';
+import BookGenerator from '../components/BookGenerator';
 
 const readerScreen = ({ navigation }) => {
     const story = navigation.getParam('story');
 
-    const NUM_OF_LINES = 25;
-    // Characters per line
-    // CPL = Width / (font-size/font-constant)
-    // 1.91 - Arial/Sans-serif font-constant
-    const CPL = 300 / (18/1.9);
-    const storyPartsLength = Math.round(story.length/CPL*NUM_OF_LINES);
-    const storyPartsList = [];
-   
-    let storyPart = story;
-    for(let i=1; i <= storyPartsLength; i++) {
-        if(storyPart.slice(0,CPL*NUM_OF_LINES) != "") {
-            storyPartsList.push(
-                <View style={styles.viewStyle} key={i}>
-                    <Text style={styles.textContent}>{storyPart.slice(0,CPL*NUM_OF_LINES)}</Text>
-                    <Text style={{color: 'gray',alignSelf:'flex-end'}}>Page {i}</Text>
-                </View>
-            );
-            storyPart = storyPart.slice(CPL*NUM_OF_LINES);
-        }
-    }
-    // console.log(`CPL is ${Math.round(CPL)}`);
-    // console.log(`Text Length - ${story.length}`);
-    // console.log(storyPartsList);
-    // console.log(storyPartsList.length);
-
     return (
-        <ViewPager style={styles.viewPager} initialPage={0}>
-            {storyPartsList}
-            {/* <View style={styles.viewStyle} key="1">
-                <Text style={styles.textContent} onTextLayout={onTextLayout}>{storyPartsList[0]}</Text>
-            </View>
-            <View key="2">
-                <Text style={styles.textContent}>{storyPartsList[1]}</Text>
-            </View>
-            <View key="3">
-                <Text style={styles.textContent}>{storyPartsList[2]}</Text>
-            </View>
-            <View key="4">
-                <Text style={styles.textContent}>{storyPartsList[3]}</Text>
-            </View> */}
-        </ViewPager>
+        <BookGenerator book={story} />
     );
 };
 
-const styles = StyleSheet.create({
-    viewPager: {
-        width: 400,
-        flex: 1,
-    },
-    viewStyle: {
-        padding: 15,
-    },
-    textContent: {
-        fontFamily: 'sans-serif',
-        fontSize: 18,
-        textAlign: 'justify',
-    }
-});
+const styles = StyleSheet.create({});
 
 export default readerScreen;
