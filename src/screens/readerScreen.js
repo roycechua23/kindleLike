0fa11a03e9b639436ce798aea3cd510c2ef09522
roycechua23 <royce236@gmail.com>
2020-07-23 16:22:37 +0800

    Add ViewPager component to readerScreen

--- src/screens/readerScreen.js
+++ src/screens/readerScreen.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ViewPager from '@react-native-community/viewpager';
 
 const readerScreen = () => {
     return (
-        <View>
-            <Text>Reader Screen</Text>
-            {/* TODO#1: Render the text/book content here. */}
-            <Text>Display book content here!</Text>
-        </View>
+        <ViewPager style={styles.viewPager} initialPage={0}>
+            <View key="1">
+                <Text>First page</Text>
+            </View>
+            <View key="2">
+                <Text>Second page</Text>
+            </View>
+        </ViewPager>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    viewPager: {
+        flex: 1,
+    },
+});
 
 export default readerScreen;
